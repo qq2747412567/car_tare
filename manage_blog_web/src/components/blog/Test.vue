3a9eb5175fbf86771ddffd5958680aa6d4e17b70
zhangqiaoyue <zhangjiaoy@newland.com.cn>
2023-06-27 17:58:27 +0800

    博客管理

---
+++ manage_blog_web/src/components/blog/Test.vue
@@ -0,0 +1,17 @@
+<template>
+ <div>
+   <div id="main">
+     <mavon-editor v-model="value"/>
+   </div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'Test'
+}
+</script>
+
+<style scoped>
+
+</style>
